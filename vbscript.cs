1686863054b384e7ffca9958fec2916785f64234
jen-ho <thaiduyenh@gmail.com>
2018-03-24 09:10:36 -0400

    add assets

---
+++ vbscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vButtonObject;
+	// Use this for initialization
+	void Start () {
+		vButtonObject = GameObject.Find ("actionButton");
+		vButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("pressed");
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		
+	}
+}
